e2057bc831db148434707921a2ae630c66418812
Sia <siavash_p@hotmail.com>
2025-04-24 14:47:50 -0700

    Changed access-control-Allow-Origin value to a wild card

--- netlify/functions/marketPerformance.ts
+++ netlify/functions/marketPerformance.ts
@@ -85,7 +85,7 @@ const handler: Handler = async (event, context) => {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': (isAllowedOrigin ? origin : ''), 
+        'Access-Control-Allow-Origin': '*', 
         'Access-Control-Allow-Methods': 'GET', 
         'Access-Control-Allow-Headers': 'Content-Type', 
       },
