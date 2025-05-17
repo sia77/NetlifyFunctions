d09fc48d1c48cb60ffd02dba9d02cb8191ce3982
Sia <siavash_p@hotmail.com>
2025-05-17 15:33:34 -0700

    Added JWT to authenticate, and authorize API endpoints as needed.

---
+++ netlify/functions/secure-api.ts
@@ -0,0 +1,25 @@
+
+const jwt = require('jsonwebtoken');
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+
+const handler: Handler = async (event:APIGatewayEvent) => {
+  const authHeader = event.headers.authorization;
+  if (!authHeader) return { 
+    statusCode: 401, 
+    body: JSON.stringify({ message:"Unauthorized" }) 
+  };
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.AUTH0_CLIENT_SECRET);
+    return { 
+      statusCode: 200, 
+      body: JSON.stringify({ user: decoded }) };
+  } catch (error) {
+    return { 
+      statusCode: 403, 
+      body: JSON.stringify({ message:"Invalid token" }) };
+  }
+};
+
+export {handler};
