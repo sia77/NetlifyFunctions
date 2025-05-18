5fb370e31cfcd6b6ee079fab1c29519048b0042b
Sia <siavash_p@hotmail.com>
2025-05-17 22:08:26 -0700

    created header constants, and using them for secure-api.ts

--- netlify/functions/secure-api.ts
+++ netlify/functions/secure-api.ts
@@ -1,6 +1,7 @@
 
 const jwt = require('jsonwebtoken');
 import { APIGatewayEvent, Handler } from 'aws-lambda';
+import { getHeaders } from '../types/constants';
 
 const handler: Handler = async (event:APIGatewayEvent) => {
   const authHeader = event.headers.authorization;
@@ -13,11 +14,13 @@ const handler: Handler = async (event:APIGatewayEvent) => {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.AUTH0_CLIENT_SECRET);
     return { 
-      statusCode: 200, 
+      statusCode: 200,
+      headers:getHeaders, 
       body: JSON.stringify({ user: decoded }) };
   } catch (error) {
     return { 
       statusCode: 403, 
+      headers:getHeaders,
       body: JSON.stringify({ message:"Invalid token" }) };
   }
 };
---
+++ netlify/types/constants.ts
@@ -0,0 +1,14 @@
+
+export const getHeaders ={headers:{
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET', 
+    'Access-Control-Allow-Headers': 'Content-Type',
+}}
+
+export const postHeaders ={headers:{
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST', 
+    'Access-Control-Allow-Headers': 'Content-Type',
+}}
